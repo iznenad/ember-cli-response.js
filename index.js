289ec9de0b95f4835dc2a0328cc08ff5edaeaa7d
Nenad Nikolic <nenad.nikolic@ebf.de>
2018-01-26 14:48:14 +0100

    create addon

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 /* eslint-env node */
 'use strict';
 
+var path = require('path');
+var Funnel = require('broccoli-funnel');
+var MergeTrees = require('broccoli-merge-trees');
+
 module.exports = {
-  name: 'ember-cli-responsejs'
+  name: 'ember-cli-responsejs',
+  included(app) {
+  	app.import('vendor/response.min.js');
+  },
+  treeForVendor(vendorTree) {
+  	let responsejsFolder = path.dirname(require.resolve('response.js/response.js'))
+  	let responsejs = new Funnel(responsejsFolder, {
+  		files: ['response.min.js']
+  	});
+
+  	return new MergeTrees([vendorTree, responsejs].filter(Boolean));
+  }
 };
